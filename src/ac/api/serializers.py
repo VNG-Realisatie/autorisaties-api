0a1afd97a4aef2eeef2baf2acbde0ebb0a918fde
annashamray <anna.shamray@maykinmedia.nl>
2019-04-23 12:50:15 +0200

    fix admin template and create api

--- src/ac/api/serializers.py
+++ src/ac/api/serializers.py
@@ -1,19 +1,61 @@
 import logging
 
+from django.db import transaction
 from rest_framework import serializers
+
+from vng_api_common.authorizations.models import Autorisatie, Applicatie
+
 
 logger = logging.getLogger(__name__)
 
 
-class ExampleSerializer(serializers.HyperlinkedModelSerializer):
+class AutorisatieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = ...
+        model = Autorisatie
+        fields = (
+            'component',
+            'zaaktype',
+            'scopes',
+            'maximale_vertrouwelijkheidaanduiding',
+        )
+
+
+class ApplicatieSerializer(serializers.HyperlinkedModelSerializer):
+    autorisaties = AutorisatieSerializer(many=True)
+
+    class Meta:
+        model = Applicatie
         fields = (
             'url',
-            ...
+            'client_ids',
+            'label',
+            'autorisaties'
         )
-        extra_kwarsg = {
+        extra_kwargs = {
             'url': {
                 'lookup_field': 'uuid',
             },
         }
+
+    @transaction.atomic
+    def create(self, validated_data):
+        autorisaties_data = validated_data.pop('autorisaties')
+        applicatie = super().create(validated_data)
+
+        for auth in autorisaties_data:
+            Autorisatie.objects.create(**auth, applicatie=applicatie)
+
+        return applicatie
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        autorisaties_data = validated_data.pop('autorisaties')
+        applicatie = super().update(instance, validated_data)
+
+        # in case of update autorisaties - remove all related autorisaties
+        if autorisaties_data:
+            applicatie.autorisaties.all().delete()
+            for auth in autorisaties_data:
+                Autorisatie.objects.create(**auth, applicatie=applicatie)
+
+        return applicatie
