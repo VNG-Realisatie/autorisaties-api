da6d9fbf01490c4525d73fd14536180e9e27b4a3
annashamray <anna.shamray@maykinmedia.nl>
2019-04-24 11:42:39 +0200

    modify tests and generate schema

--- src/ac/api/tests/test_authorizations_api.py
+++ src/ac/api/tests/test_authorizations_api.py
@@ -4,8 +4,9 @@ from vng_api_common.constants import VertrouwelijkheidsAanduiding
 from vng_api_common.tests import (
     JWTScopesMixin, get_operation_url, get_validation_errors
 )
+from vng_api_common.authorizations.models import Applicatie, Autorisatie
 
-from ac.datamodel.tests.factories import ApplicatieFactory
+from ac.datamodel.tests.factories import ApplicatieFactory, AutorisatieFactory
 
 
 class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
@@ -16,20 +17,24 @@ class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
 
         All authorizations should be granted because of the flag provided. This
         gives an option to do coarse-grained authorization for an application.
-
-        TODO: add assertions on the underlying model instances.
         """
         url = get_operation_url('applicatie_create')
 
         data = {
             'client_ids': ['id1', 'id2'],
             'label': 'Melding Openbare Ruimte consumer',
-            'heeftAlleAutorisatie': True,
+            'heeftAlleAutorisaties': True,
         }
 
         response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        applicatie = Applicatie.objects.get()
+
+        self.assertEqual(applicatie.client_ids, ['id1', 'id2'])
+        self.assertEqual(applicatie.label, 'Melding Openbare Ruimte consumer')
+        self.assertEqual(applicatie.heeft_alle_autorisaties, True)
 
     def test_create_application_with_detail_permissions(self):
         """
@@ -38,8 +43,6 @@ class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
         Fine-grained authorization can be achieved per ZaakType, which limits
         which scopes are allowed for this particular type. The same applies
         for maxVetrouwelijkheidaanduiding.
-
-        TODO: add assertions on the underlying model instances.
         """
         url = get_operation_url('applicatie_create')
 
@@ -53,7 +56,7 @@ class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
                     'zds.scopes.zaken.aanmaken',
                 ],
                 'zaaktype': 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/1/zaaktypen/1',
-                'maxVetrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.beperkt_openbaar,
+                'maxVertrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.beperkt_openbaar,
             }, {
                 'component': 'ZRC',
                 'scopes': [
@@ -62,13 +65,40 @@ class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
                     'zds.scopes.zaken.verwijderen',
                 ],
                 'zaaktype': 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/2/zaaktypen/1',
-                'maxVetrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.zeer_geheim,
+                'maxVertrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.zeer_geheim,
             }],
         }
 
         response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        applicatie = Applicatie.objects.get()
+        autorisaties = Autorisatie.objects.order_by('zaaktype').all()
+
+        self.assertEqual(applicatie.client_ids, ['id1', 'id2'])
+        self.assertEqual(applicatie.label, 'Melding Openbare Ruimte consumer')
+        self.assertEqual(applicatie.heeft_alle_autorisaties, False)
+        self.assertEqual(len(autorisaties), 2)
+
+        auth1, auth2 = autorisaties
+
+        self.assertEqual(auth1.applicatie, applicatie)
+        self.assertEqual(auth1.component, 'ZRC')
+        self.assertEqual(auth1.zaaktype, 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/1/zaaktypen/1')
+        self.assertEqual(
+            auth1.scopes,
+            ['zds.scopes.zaken.lezen', 'zds.scopes.zaken.aanmaken']
+        )
+        self.assertEqual(auth1.max_vertrouwelijkheidaanduiding, VertrouwelijkheidsAanduiding.beperkt_openbaar)
+        self.assertEqual(auth2.applicatie, applicatie)
+        self.assertEqual(auth2.component, 'ZRC')
+        self.assertEqual(
+            auth2.scopes,
+            ['zds.scopes.zaken.lezen', 'zds.scopes.zaken.aanmaken', 'zds.scopes.zaken.verwijderen']
+        )
+        self.assertEqual(auth2.zaaktype, 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/2/zaaktypen/1')
+        self.assertEqual(auth2.max_vertrouwelijkheidaanduiding, VertrouwelijkheidsAanduiding.zeer_geheim)
 
     def test_create_all_permissions_and_explicitly_provided(self):
         """
@@ -81,7 +111,7 @@ class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
         data = {
             'client_ids': ['id1', 'id2'],
             'label': 'Melding Openbare Ruimte consumer',
-            'heeftAlleAutorisatie': True,
+            'heeftAlleAutorisaties': True,
             'autorisaties': [{
                 'component': 'ZRC',
                 'scopes': [
@@ -89,7 +119,7 @@ class SetAuthorizationsTests(JWTScopesMixin, APITestCase):
                     'zds.scopes.zaken.aanmaken',
                 ],
                 'zaaktype': 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/1/zaaktypen/1',
-                'maxVetrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.beperkt_openbaar,
+                'maxVertrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.beperkt_openbaar,
             }],
         }
 
@@ -146,17 +176,17 @@ class ReadAuthorizationsTests(JWTScopesMixin, APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        ApplicatieFactory.create(
-            client_ids=['id1', 'id2'],
-            autorisaties__zaaktype='https://example.com',
-            autorisaties__scopes=['dummy.scope'],
-            autorisaties__max_vetrouwelijkheidaanduiding=VertrouwelijkheidsAanduiding.openbaar,
+        AutorisatieFactory.create(
+            applicatie__client_ids=['id1', 'id2'],
+            zaaktype='https://example.com',
+            scopes=['dummy.scope'],
+            max_vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduiding.openbaar,
         )
 
     def test_filter_client_id_hit(self):
         url = get_operation_url('applicatie_list')
 
-        response = self.client.get(url, {'client_id': 'id2'})
+        response = self.client.get(url, {'client_ids': 'id2'})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 1)
@@ -164,7 +194,7 @@ class ReadAuthorizationsTests(JWTScopesMixin, APITestCase):
     def test_filter_client_id_miss(self):
         url = get_operation_url('applicatie_list')
 
-        response = self.client.get(url, {'client_id': 'id3'})
+        response = self.client.get(url, {'client_ids': 'id3'})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 0)
@@ -174,12 +204,13 @@ class UpdateAuthorizationsTests(JWTScopesMixin, APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.applicatie = ApplicatieFactory.create(
-            client_ids=['id1', 'id2'],
-            autorisaties__zaaktype='https://example.com',
-            autorisaties__scopes=['dummy.scope'],
-            autorisaties__max_vetrouwelijkheidaanduiding=VertrouwelijkheidsAanduiding.openbaar,
+        autorisatie = AutorisatieFactory.create(
+            applicatie__client_ids=['id1', 'id2'],
+            zaaktype='https://example.com',
+            scopes=['dummy.scope'],
+            max_vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduiding.openbaar,
         )
+        cls.applicatie = autorisatie.applicatie
 
     def test_update_client_ids(self):
         url = get_operation_url('applicatie_partial_update', uuid=self.applicatie.uuid)
@@ -187,6 +218,9 @@ class UpdateAuthorizationsTests(JWTScopesMixin, APITestCase):
         response = self.client.patch(url, {'client_ids': ['id1']})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.applicatie.refresh_from_db()
+        self.assertEqual(self.applicatie.client_ids, ['id1'])
 
     def test_replace_authorizations(self):
         url = get_operation_url('applicatie_partial_update', uuid=self.applicatie.uuid)
@@ -197,7 +231,7 @@ class UpdateAuthorizationsTests(JWTScopesMixin, APITestCase):
                     'zds.scopes.zaken.lezen',
                     'zds.scopes.zaken.aanmaken',
                 ],
-                'maxVetrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.beperkt_openbaar,
+                'maxVertrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.beperkt_openbaar,
                 'zaaktype': 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/1/zaaktypen/1',
             }, {
                 'component': 'ZRC',
@@ -207,10 +241,28 @@ class UpdateAuthorizationsTests(JWTScopesMixin, APITestCase):
                     'zds.scopes.zaken.verwijderen',
                 ],
                 'zaaktype': 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/2/zaaktypen/1',
-                'maxVetrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.zeer_geheim,
+                'maxVertrouwelijkheidaanduiding': VertrouwelijkheidsAanduiding.zeer_geheim,
             }],
         }
 
         response = self.client.patch(url, data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.applicatie.autorisaties.count(), 2)
+
+        auth1, auth2 = self.applicatie.autorisaties.order_by('zaaktype').all()
+
+        self.assertEqual(auth1.zaaktype, 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/1/zaaktypen/1')
+        self.assertEqual(auth1.max_vertrouwelijkheidaanduiding, VertrouwelijkheidsAanduiding.beperkt_openbaar)
+        self.assertEqual(auth2.zaaktype, 'https://ref.tst.vng.cloud/zrc/api/v1/catalogus/2/zaaktypen/1')
+        self.assertEqual(auth2.max_vertrouwelijkheidaanduiding, VertrouwelijkheidsAanduiding.zeer_geheim)
+
+    def test_update_authorization_incorrect(self):
+        url = get_operation_url('applicatie_partial_update', uuid=self.applicatie.uuid)
+
+        response = self.client.patch(url, {'heeftAlleAutorisaties': True})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        error = get_validation_errors(response, 'nonFieldErrors')
+        self.assertEqual(error['code'], 'ambiguous-authorizations-specified')
--- src/resources.md
+++ src/resources.md
@@ -4,5 +4,28 @@ Dit document beschrijft de (RGBZ-)objecttypen die als resources ontsloten
 worden met de beschikbare attributen.
 
 
+## Autorisatie
+
+Objecttype op [GEMMA Online](https://www.gemmaonline.nl/index.php/Rgbz_1.0/doc/objecttype/autorisatie)
+
+| Attribuut | Omschrijving | Type | Verplicht | CRUD* |
+| --- | --- | --- | --- | --- |
+| component | Name of the component to authorize | string | ja | C​R​U​D |
+| zaaktype | Url of the zaaktype that is allowed | string | ja | C​R​U​D |
+| scopes | Comma-separated list of identifiers used for authentication | array | ja | C​R​U​D |
+| maxVertrouwelijkheidaanduiding | Maximum level of confidentiality that is allowed | string | ja | C​R​U​D |
+
+## Applicatie
+
+Objecttype op [GEMMA Online](https://www.gemmaonline.nl/index.php/Rgbz_1.0/doc/objecttype/applicatie)
+
+| Attribuut | Omschrijving | Type | Verplicht | CRUD* |
+| --- | --- | --- | --- | --- |
+| url |  | string | nee | ~~C~~​R​~~U~~​~~D~~ |
+| clientIds | Comma-separated list of identifiers used for authentication | array | ja | C​R​U​D |
+| label | A human readable representation of the application | string | ja | C​R​U​D |
+| heeftAlleAutorisaties | Globally allows everything | boolean | nee | C​R​U​D |
+| autorisaties |  | array | nee | C​R​U​D |
+
 
 * Create, Read, Update, Delete
